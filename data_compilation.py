f99bd0c2dc54c37375f8a9cde15383dc28bcc6c8
MariajoseFranco <mariajosefranco@Mariajoses-MacBook-Pro.local>
2025-06-11 11:36:13 +0200

    documenting ad typing data compilation script

--- data_compilation.py
+++ data_compilation.py
@@ -1,24 +1,31 @@
-from typing import Union
-
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 
 class DataCompilation():
+    """
+        Initialize the DataCompilation class with paths to input and output data.
+
+        Args:
+            data_path (str): Path to the directory containing interaction datasets.
+            disease_path (str): Path to the CSV file with selected diseases (by CUI).
+            output_path (str): Path where processed data will be saved.
+        """
     def __init__(self, data_path, disease_path, output_path) -> None:
         self.data_path = data_path
         self.disease_path = disease_path
         self.output_path = output_path
 
-    def get_data(self) -> Union[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    def get_data(self) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         """
-        Retrieve and clean the data from the specified paths.
+        Executes the data getter pipeline for all three datasets:
+        protein-protein, gene-protein, and disease-gene interactions.
 
         Returns:
             tuple:
-                - pd.DataFrame: Protein-Protein interaction data.
-                - pd.DataFrame: Gene-Protein interaction data.
-                - pd.DataFrame: Disease-Gene interaction data.
+                - pd.DataFrame: Cleaned protein-protein interaction data.
+                - pd.DataFrame: Cleaned gene-protein interaction data.
+                - pd.DataFrame: Cleaned disease-gene interaction data.
         """
         df_pro_pro = self.get_and_clean_pro_pro_data()
         df_gen_pro = self.get_and_clean_gen_pro_data()
@@ -26,10 +33,12 @@ class DataCompilation():
         return df_pro_pro, df_gen_pro, df_dis_gen
 
     def get_and_clean_pro_pro_data(self) -> pd.DataFrame:
-        """_summary_
+        """
+        Load and clean protein-protein interaction data by removing duplicates
+        and self-interactions.
 
         Returns:
-            pd.DataFrame: _description_
+            pd.DataFrame: Cleaned protein-protein interaction data.
         """
         df_ppi = pd.read_csv(f'{self.data_path}/pro_pro.tsv', sep='\t')
 
@@ -44,78 +53,75 @@ class DataCompilation():
         return df_pro_pro
 
     def get_and_clean_gen_pro_data(self) -> pd.DataFrame:
-        """_summary_
+        """
+        Load and clean gene-protein interaction data by removing duplicates.
 
         Returns:
-            pd.DataFrame: _description_
+            pd.DataFrame: Cleaned gene-protein interaction data.
         """
         df_gen_pro = pd.read_csv(f'{self.data_path}/gen_pro.tsv', sep='\t')
         df_gen_pro = df_gen_pro.drop_duplicates()
         return df_gen_pro
 
     def get_and_clean_dis_gen_data(self) -> pd.DataFrame:
-        """_summary_
+        """
+        Load and clean disease-gene interaction data by removing duplicate disease-gene mappings.
 
         Returns:
-            pd.DataFrame: _description_
+            pd.DataFrame: Cleaned disease-gene interaction data.
         """
         df_dis_gen = pd.read_csv(f'{self.data_path}/dis_gen.tsv', sep='\t')
         df_dis_gen = df_dis_gen.drop_duplicates(subset=['disease_name', 'gene_id', 'gene_symbol'])
         return df_dis_gen
 
-    def get_dis_pro_data(self, df_dis_gen, df_gen_pro):
+    def get_dis_pro_data(self, df_dis_gen: pd.DataFrame, df_gen_pro: pd.DataFrame) -> pd.DataFrame:
         """
-        Merge the disease-gene and gene-protein datasets to create disease-protein interactions.
-        Filters the data to include only the diseases matched with the selected list.
+        Merge disease-gene and gene-protein data to obtain disease-protein interactions.
+        Filters out genes that do not code for proteins.
 
         Args:
             df_dis_gen (pd.DataFrame): Disease-Gene interaction data.
             df_gen_pro (pd.DataFrame): Gene-Protein interaction data.
-            selected_diseases (pd.DataFrame): Preprocessed diseases to match.
-            output_path (str): Path were the outputs are going to be stored.
 
         Returns:
-            tuple:
-                - pd.DataFrame: Filtered disease-protein interaction data.
-                - list: List of matched disease names.
+            pd.DataFrame: Merged and filtered disease-protein interaction data.
         """
         df_dis_pro = df_dis_gen.merge(
             df_gen_pro, how='left', on='gene_id', indicator=True
         )
         df_dis_pro = df_dis_pro[df_dis_pro['_merge'] == 'both'].drop(
             ['_merge'], axis=1
-        )  # se eliminan los genes que no codifican proteina (no salen en df_gen_pro)
+        )
         return df_dis_pro
 
-    def encoding_diseases(self, df_dis_pro):
+    def encoding_diseases(self, df_dis_pro: pd.DataFrame) -> pd.DataFrame:
         """
-        Encode disease names into integer IDs using LabelEncoder.
+        Encode disease names into integer IDs using sklearn's LabelEncoder.
 
         Args:
-            df_dis_pro (pd.DataFrame): Disease-Protein interaction
-            data with a 'disease_name' column.
+            df_dis_pro (pd.DataFrame): Disease-protein interaction data.
 
         Returns:
-            pd.DataFrame: DataFrame with a new column 'disease_id'.
+            pd.DataFrame: DataFrame with an added 'disease_id' column with the diseases encoded.
         """
         disease_encoder = LabelEncoder()
         df_dis_pro['disease_id'] = disease_encoder.fit_transform(df_dis_pro['disease_name'])
         return df_dis_pro
 
-    def encoding_proteins(self, df_pro_pro, df_dis_pro):
+    def encoding_proteins(
+            self, df_pro_pro: pd.DataFrame, df_dis_pro: pd.DataFrame
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
-        Encode protein identifiers into integer IDs using LabelEncoder.
+        Encode protein identifiers into integer IDs using sklearn's LabelEncoder.
 
         Args:
-            df_pro_pro (pd.DataFrame): Protein-Protein interaction data with columns ['prA', 'prB'].
-            df_dis_pro (pd.DataFrame): Disease-Protein interaction data with column 'protein_id'.
+            df_pro_pro (pd.DataFrame): Protein-protein interaction data.
+            df_dis_pro (pd.DataFrame): Disease-protein interaction data.
 
         Returns:
             tuple:
-                - pd.DataFrame: Encoded protein-protein interaction data
-                with 'src_id' and 'dst_id' columns.
-                - pd.DataFrame: Encoded disease-protein interaction data
-                with 'protein_id_enc' column.
+                - pd.DataFrame: Protein-protein interaction data with 'src_id' and 'dst_id' columns.
+                - pd.DataFrame: Disease-protein interaction data with 'protein_id_enc' column.
         """
         protein_encoder = LabelEncoder()
         all_proteins = pd.concat([df_pro_pro['prA'], df_pro_pro['prB'], df_dis_pro['protein_id']])
@@ -125,38 +131,60 @@ class DataCompilation():
         df_dis_pro['protein_id_enc'] = protein_encoder.transform(df_dis_pro['protein_id'])
         return df_pro_pro, df_dis_pro
 
-    def get_matched_diseases(self, df_dis_pro):
+    def get_matched_diseases(self, df_dis_pro: pd.DataFrame) -> tuple[pd.DataFrame, list[str]]:
         """
-        Load and clean the disease names from a CSV file.
+        Filter disease-protein interactions to keep only selected diseases (based on CUI list).
+        Also writes the matched disease names to a CSV file.
 
-        The CSV is expected to have one column without a header.
-        A new column 'disease_cleaned' is added with preprocessed disease names.
+        Args:
+            df_dis_pro (pd.DataFrame): Encoded disease-protein interaction data.
+            Must include a 'cui' and 'disease_name' column.
 
         Returns:
-            pd.DataFrame: DataFrame with columns ['DISEASE', 'disease_cleaned'].
+            tuple:
+                - pd.DataFrame: Filtered disease-protein interaction data for matched diseases.
+                - list[str]: List of matched disease names.
         """
         diseases = pd.read_csv(self.disease_path)
         cui_list = diseases['cui'].to_list()
         df_dis_pro_matched = df_dis_pro[df_dis_pro['cui'].isin(cui_list)]
+        selected_diseases = df_dis_pro_matched['disease_name'].unique().tolist()
+        self.save_matched_diseases_to_csv()
+        return df_dis_pro_matched, selected_diseases
+
+    def save_matched_diseases_to_csv(self, df_dis_pro_matched: pd.DataFrame) -> None:
+        """
+        Save the list of unique matched disease names to a CSV file.
+
+        The file will be created in the output path specified during initialization,
+        and will include a header followed by the disease names.
+
+        Args:
+            df_dis_pro_matched (pd.DataFrame): DataFrame containing disease-protein interactions
+            for the matched diseases.
+
+        Returns:
+            None
+        """
         with open(
             f"{self.output_path}/diseases_of_interest.csv", "w"
         ) as f:
             f.write("DISEASES OF INTEREST\n")
             for disease in df_dis_pro_matched['disease_name'].unique():
                 f.write(f"{disease}\n")
-        selected_diseases = df_dis_pro_matched['disease_name'].unique().tolist()
-        return df_dis_pro_matched, selected_diseases
 
-    def removing_duplicated_proteins(self, df_dis_pro, selected_diseases):
+    def removing_duplicated_proteins(
+            self, df_dis_pro: pd.DataFrame, selected_diseases: list[str]
+    ) -> pd.DataFrame:
         """
-        Remove duplicated
+        Remove duplicate protein associations for each disease by keeping the highest scoring one.
 
         Args:
-            df_dis_pro (pd.DataFrame): DataFrame containing the disease-protein
-            associations.
+            df_dis_pro (pd.DataFrame): Disease-protein interaction data.
+            selected_diseases (list[str]): List of disease names to process.
 
         Returns:
-            pd.DataFrame: DataFrame with duplicated interactions removed.
+            pd.DataFrame: Disease-protein data with duplicates removed per disease.
         """
         results = []
         for disease in selected_diseases:
@@ -169,26 +197,22 @@ class DataCompilation():
         df_dis_pro_without_duplicated_proteins = pd.concat(results, ignore_index=True)
         return df_dis_pro_without_duplicated_proteins
 
-    def main(self) -> Union[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    def main(self) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, list[str]]:
         """
-        Executes the data preparation pipeline:
-            - Loads interaction data.
-            - Merges disease-gene and gene-protein interactions into disease-protein associations.
-            - Encodes disease and protein IDs.
-            - Returns the final encoded datasets.
-
-        Args:
-            selected_diseases (pd.DataFrame): DataFrame containing the preprocessed
-            selected diseases.
-            output_path (str): Path were the outputs are going to be stored
+        Run the full data compilation and processing pipeline:
+            - Load raw interaction datasets.
+            - Merge disease-gene and gene-protein interactions into disease-protein.
+            - Encode diseases and proteins.
+            - Filter interactions to retain only selected diseases.
+            - Remove duplicated protein associations.
 
         Returns:
             tuple:
-                - df_pro_pro_encoded (pd.DataFrame): Encoded protein-protein interactions.
-                - df_gen_pro (pd.DataFrame): Gene-Protein interactions.
-                - df_dis_gen (pd.DataFrame): Disease-Gene interactions.
-                - df_dis_pro_encoded (pd.DataFrame): Encoded disease-protein associations.
-                - diseases_matched (list): List of matched disease names.
+                - pd.DataFrame: Encoded protein-protein interaction data.
+                - pd.DataFrame: Cleaned gene-protein interaction data.
+                - pd.DataFrame: Cleaned disease-gene interaction data.
+                - pd.DataFrame: Final encoded disease-protein interaction data.
+                - list[str]: List of matched disease names.
         """
         df_pro_pro, df_gen_pro, df_dis_gen = self.get_data()
         df_dis_pro = self.get_dis_pro_data(df_dis_gen, df_gen_pro)
